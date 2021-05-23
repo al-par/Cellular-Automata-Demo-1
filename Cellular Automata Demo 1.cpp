d4aabba477681d3fc76bac40518bfd5124da01f2
JonTronsLongJon <47093296+JonTronsLongJon@users.noreply.github.com>
2021-05-23 04:25:43 -0400

    Initialized relevant parts of Allegro 5

--- Cellular Automata Demo 1.cpp
+++ Cellular Automata Demo 1.cpp
@@ -11,9 +11,9 @@
 #include <allegro5/allegro_primitives.h>
 
 
-#define DISPLAY_WIDTH 1920 //This is also used instead of al_get_display_width to save on processing time (I know these savings are minuscule, but whatever)
+#define DISPLAY_WIDTH 1920
 #define DISPLAY_HEIGHT 1080
-#define FRAMERATE 60
+#define FRAMERATE 60 //This value is in frames per second
 
 
 using namespace std;
@@ -21,5 +21,44 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	srand(time(NULL));
+	int returnCode = 0;
+
+	//___________________________________________________________________________________________________________________________________________________________________
+	//initialize allegro 5
+
+	ALLEGRO_DISPLAY* display;
+	ALLEGRO_EVENT_QUEUE* queue;
+	ALLEGRO_TIMER* timer;
+
+	al_init();
+	al_init_font_addon();
+	al_init_ttf_addon();
+	al_install_keyboard();
+	al_install_mouse();
+	al_init_primitives_addon();
+
+	display = al_create_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	queue = al_create_event_queue();
+	timer = al_create_timer(1.0 / FRAMERATE);
+
+	al_register_event_source(queue, al_get_keyboard_event_source());
+	al_register_event_source(queue, al_get_display_event_source(display));
+	al_register_event_source(queue, al_get_mouse_event_source());
+	al_register_event_source(queue, al_get_timer_event_source(timer));
+
+	al_start_timer(timer);
+
+
+
+	//___________________________________________________________________________________________________________________________________________________________________
+	//destructors and cleanup
+
+	al_destroy_display(display);
+	al_uninstall_keyboard();
+	al_uninstall_mouse();
+	al_destroy_timer(timer);
+
+	//end of program
+	return returnCode;
 }
