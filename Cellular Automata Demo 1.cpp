d3854fa8e221545e8c251763616ef3c1f3862fae
JonTronsLongJon <47093296+JonTronsLongJon@users.noreply.github.com>
2021-05-23 04:15:22 -0400

    Included and activated relevant header files

--- Cellular Automata Demo 1.cpp
+++ Cellular Automata Demo 1.cpp
@@ -1,6 +1,23 @@
+//standard includes
 #include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+//Allegro 5 includes
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+
+
+#define DISPLAY_WIDTH 1920 //This is also used instead of al_get_display_width to save on processing time (I know these savings are minuscule, but whatever)
+#define DISPLAY_HEIGHT 1080
+#define FRAMERATE 60
+
 
 using namespace std;
+
 
 int main()
 {
